800dc3a84b5bd13edb6aff244bd750fc6c5dba43
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-03 02:13:53 +0000

    Add variaveis string

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a="This is the \u0009 first string \u000A";
+            string b=@"This is the \t second string \n";
+            string c="\"This is the third string\"";
+            string d=@"""This is the fifth string""";
+            Console.WriteLine(a+"\u000A"+b+"\u000A"+c+"\u000A"+d);
+        }
+    }
+}
