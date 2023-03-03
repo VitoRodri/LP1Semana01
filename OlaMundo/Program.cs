909bfe49ab4cbde7033d5d565d790d71850d461b
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-03 00:59:32 +0000

    Add comentários

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,10 @@
 
 namespace OlaMundo
 {
+    /// First class
     class Program
     {
+        /// Main loop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
