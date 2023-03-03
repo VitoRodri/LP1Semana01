17932f8088a55f81ee60607a63e3f06be6dab83d
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-03 11:32:08 +0000

    Add ArgInt2Hex

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArgInt2Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number");
+            string input =Console.ReadLine();
+            int number;
+          
+            bool Parsabel =Int32.TryParse(input, out number);
+            if (Parsabel)
+            {
+                Console.WriteLine("Number is  int");
+                Console.WriteLine("Hexadecimal: {0:x}",number);
+            }
+            else
+            {
+                Console.WriteLine("Number is not int");
+            }
+            
+        }
+    }
+}
