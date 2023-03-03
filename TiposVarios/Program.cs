201b5a071bd6194e05beb246318907c124ce1cbf
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-03 01:28:19 +0000

    Add variaveis inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2;
+            uint b=120U;
+            long c=-32L;
+            ulong d=40UL;
+            
+            Console.WriteLine(a+","+b+","+c+","+d);
+
+        }
+    }
+}
